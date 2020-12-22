99a5e436905f417a96597738c48032178c993921
lihuaiyi01 <1195841709@qq.com>
2020-12-22 17:39:40 +0800

    111

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const name = "React";
+const obj = {
+    firstName: 'Harry',
+    lastName: 'Potter'
+};
+function formatName(name) {
+    return name.firstName + ' ' + name.lastName
+}
+const jsx = (
+    <div>
+        <div>hello,{name}</div>
+        <div>{formatName(obj)}</div>
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(jsx, document.getElementById('root'));
